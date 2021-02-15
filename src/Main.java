de814ffada3f084033f0b8906187471b78ff4198
santima1904 <santima1904@gmail.com>
2021-02-15 14:34:35 +0100

    Commit inicial

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
               contador += matriz[i][j];
             }
         }
+        
         for(int i=0;i< matriz.length;i++){
             for (int j=0;j<matriz[i].length;j++){
                 System.out.print(matriz[i][j]+" ");
